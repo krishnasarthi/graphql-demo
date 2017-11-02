0cdbb906021c33848019d3914cffb9f33c86d41e
krishnasarthi <krishna_sarthi@yahoo.co.in>
2017-11-02 07:49:05 +0530

    added env file

--- mysql/dbConnection.js
+++ mysql/dbConnection.js
@@ -1,11 +1,12 @@
 import mysql from 'mysql';
+require('dotenv').load();
 
 const mysqlPool = mysql.createPool({
     connectionLimit: 100,
-    host: 'mysql.megameeting.com',
-    user: 'dbaloha',
-    password: 'GrandioseCrashActress',
-    database: 'megameeting',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
     connectTimeout: 3600,
     acquireTimeout: 3600
 });
